ac8dccbf055b29712cab846da9e8cc421485bcd3
haymanothaymi423-ctrl <haymanothaymi423@gmail.com>
2025-10-21 23:10:49 +0300

    Update solution.py

--- Problem_2_Unit_Converter/solution.py
+++ Problem_2_Unit_Converter/solution.py
@@ -1,1 +1,25 @@
-# your code here
+#1. Ask which conversion user wants.
+print("choose a conversion:")
+print("1.kilometer to meter:")
+print("2.meter to kilometer:")
+print("3.celsius to fahrenit:")
+print("4.fahrenit to celsius:")
+#take choice input
+choice =int(input("enter your choice (1-4):"))
+#2. Input value.
+value=float (input("enter the value to convert:"))
+#3. Convert and print result.
+if choice==1:
+  result =value*1000
+  print(f"{value:.2f}km ={result:.2f} m")
+elif choice==2:
+  result=value/1000
+  print(f"{value:.2f}m={result:.2f} km")
+elif choice==3:
+  result=(value*9/5)+32
+  print(f"{value:.2f}c={result:.2f}f")
+elif choice ==4:
+  result=(value-32)*5/9
+  print (f"{value:.2f}f={result:.2f}c")
+else:
+  print ("invalid option!please choose anumber betweeen 1 and 4")
