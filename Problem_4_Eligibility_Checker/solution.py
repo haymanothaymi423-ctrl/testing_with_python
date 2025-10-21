96c535d6d31e3e412e4e5de75ecd144fe842f603
haymanothaymi423-ctrl <haymanothaymi423@gmail.com>
2025-10-21 23:11:34 +0300

    Add eligibility checker for driving
    Implement eligibility checker for driving based on age and license status.

--- Problem_4_Eligibility_Checker/solution.py
+++ Problem_4_Eligibility_Checker/solution.py
@@ -1,1 +1,12 @@
-# your code here
+#1. Get age and license status.
+age=int(input("enter your age:"))
+has_license=input(" do you have a driving license?(yes/no):").lower()
+#2. Use nested if/else to check eligibility.
+if age>=18:
+  if has_license=="yes":
+    print(" you are eligible to drive.")
+  else:
+   print ("you are old enough, but you need a driving license.")
+else:
+ print("you are not eligible to drive yet.")
+#3. Print result.
