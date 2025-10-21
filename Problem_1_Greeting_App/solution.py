e8eb6925c7f684fbf8bb799ac3118e8f306f2788
haymanothaymi423-ctrl <haymanothaymi423@gmail.com>
2025-10-21 23:10:08 +0300

    Implement user greeting and birth year calculation
    Added user input for name, favorite food, and age, and calculated birth year.

--- Problem_1_Greeting_App/solution.py
+++ Problem_1_Greeting_App/solution.py
@@ -1,1 +1,12 @@
-# your code here
+#1. Get name, favorite food, year, and age.
+name = input ("enter your name :")
+favorite_food = input(" enter your favorite food:" )
+age=int(input("enter your age:"))
+print(f"hello {name},nice to meet you!")
+print(f"your favorite food is {favorite_food}")
+print(f"your age is {age}")
+#3. Calculate birth year (optional).
+current_year=int(input("enter current year:"))
+age=int(input("enter your age:"))
+birth_year=current_year - age
+print(f"you were born in {birth_year}.")
