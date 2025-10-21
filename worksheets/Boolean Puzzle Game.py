1b75c046d4af170c6e375b3b805535308aeb4cc0
haymanothaymi423-ctrl <haymanothaymi423@gmail.com>
2025-10-21 22:20:48 +0300

    Refactor comments into code for access system

--- worksheets/Boolean Puzzle Game.py
+++ worksheets/Boolean Puzzle Game.py
@@ -1,9 +1,11 @@
-"""
-You’re designing a gate access system. Access is granted only if:
-
-The user is verified (verified == True)
-
-The user has an even ID (id & 1 == 0)
-
-The security flag bits contain at least one 1 in the last 3 bits (flags & 0b111 != 0)
-"""
+#You’re designing a gate access system. Access is granted only if:
+#The user is verified (verified == True)
+#The user has an even ID (id & 1 == 0)
+#The security flag bits contain at least one 1 in the last 3 bits (flags & 0b111 != 0)
+verified = bool(int(input("Is user verified? (1 for Yes, 0 for No): ")))
+user_id = int(input("Enter user ID: "))
+security_flag = int(input("Enter security flag (integer): "))
+if verified and (user_id & 1 == 0) and (security_flag & 0b111 != 0):
+    print(" Access Granted")
+else:
+    print(" Access Denied")
