399afa085f7db373210a7795d8e7732b7f1f513c
haymanothaymi423-ctrl <haymanothaymi423@gmail.com>
2025-10-21 22:25:23 +0300

    Refactor comments in Parity and Power Detector
    Updated comments to clarify input and output behavior.

--- worksheets/Parity and Power Detector.py
+++ worksheets/Parity and Power Detector.py
@@ -1,10 +1,10 @@
-"""
-
-Input an integer.
-
-Print:
-
-Whether it’s even or odd using num & 1
-
-Whether it’s a power of two using (num & (num - 1)) == 0
-"""
+#Input an integer.
+#Print:
+#Whether it’s even or odd using num & 1
+#Whether it’s a power of two using (num & (num - 1)) == 0
+num = int(input("Enter a number: "))
+print(f"{num} is {'even' if num % 2 == 0 else 'odd'}")
+if num > 0 and (num & (num - 1)) == 0:
+    print(f"{num} is a power of 2")
+else:
+    print(f"{num} is not a power of 2")
