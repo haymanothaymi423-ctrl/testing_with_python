d83b2b42f4d467046e74ac81e1f57e6009395e64
haymanothaymi423-ctrl <haymanothaymi423@gmail.com>
2025-10-21 23:11:10 +0300

    Add simple calculator functionality
    Implement a simple calculator that performs addition, subtraction, multiplication, and division. Includes error handling for division by zero and invalid operations.

--- Problem_3_Simple_Calculator/solution.py
+++ Problem_3_Simple_Calculator/solution.py
@@ -1,1 +1,27 @@
-# your code here
+
+#simple calculater
+#1. Input two numbers.
+num1=float(input("enter the first number:"))
+num2=float(input("enter the second number:"))
+#2. Input operation.
+operation =input("enter the operation (/,*,-,+):")
+#3. Perform calculation.
+if operation=='+':
+   result=num1+num2
+elif operation=='-':
+  result=num1-num2
+elif operation=='*':
+  result =num1*num2
+elif operation=='/':
+#4. Handle division by zero.
+  if num2==0:
+    print("error or division by zero is not allowed")
+    result =None
+  else:
+   result=num1/num2
+else:
+  print("invalid operation")
+  result =None
+#display result if valid
+if result is not none:
+  print("result:",result)
